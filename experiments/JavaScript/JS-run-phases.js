33cc9ef9809833d3dd2f96b35625971b99fe9f6e
Luis Pomare <luispomare92@gmail.com>
2025-12-27 22:48:54 -0500

    Fix JS execution order exercise

--- experiments/JavaScript/JS-run-phases.js
+++ experiments/JavaScript/JS-run-phases.js
@@ -2,8 +2,8 @@ console.log(sayHi().name, sayHi().number, sayHi().counter);
 
 
 function sayHi() {
-  counter++
+  counter = 10
   return { name: "Hello", number: 7, counter }
 }
 
-var counter = 0
+var counter = 0 // If this declaration is changed by "let counter = 0" the execution is going to crash since let do not allow assignment before declaration
