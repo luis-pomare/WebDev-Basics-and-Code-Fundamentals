453bdcf5b7b4ed2713e5a6ffc4f30b7f1642559d
Luis Pomare <luispomare92@gmail.com>
2025-12-31 12:17:21 -0500

    Add function-scoped-vs-block-scoped-practice doccumentation

--- experiments/JavaScript/function-scoped-vs-block-scoped-practice.js
+++ experiments/JavaScript/function-scoped-vs-block-scoped-practice.js
@@ -1,12 +1,19 @@
+// Function declaration is hoisted, so it can be called
 function calculator() {
-  var x = 5
+  // `x` is declared with `var`, so it is function-scoped and accessible anywhere inside the `calculator` function
+  var x = 5;
+
+  // This `if` statement creates a block HOWEVER, `var = y` does NOT respect block scope
   if (true) {
-    var y = 3
-    var z = 0 // If I chage this var declaration for "let z = 0" then the execution is going to crash because of the block scope 
+    // y and z are hoisted to the top of the function scope
+    var y = 3;
+    var z = 0;
+
+    // If this line were changed to let z = 0 then `z` would be block-scoped and NOT accessible outside this `if` block
   }
-  console.log(x, y, z)
+
+  // At this point: x, y, and z all exist in the same function scope because they were declared using `var`
+  console.log(x, y, z);
 }
 
-
-
-calculator()
+calculator();
