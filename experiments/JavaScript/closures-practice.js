a19fc1c0e7af195380531645e38d57923b366ffe
Luis Pomare <luispomare92@gmail.com>
2026-01-01 13:41:24 -0500

    Add closures practice doccumentation

--- experiments/JavaScript/closures-practice.js
+++ experiments/JavaScript/closures-practice.js
@@ -1,11 +1,15 @@
-// Returns a function that closes over `pi`, allowing it to be reused across calls without redefinition.
+
 function envelop() {
+  // pi will be captured by the inner function returned below
   const pi = 3.14159
+
   return function calculator(radius) {
     return 2 * radius * pi
   }
 }
 
+// `pi` is created and captured in a closure, the returned function is stored in `circumferenceCalculator`
 const circumferenceCalculator = envelop()
 
+// Each call to circumferenceCalculator() creates a new execution context for calculator(), and uses the same `pi` value from the closure
 console.log(circumferenceCalculator(3), circumferenceCalculator(0.159155078))
