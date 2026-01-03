e88589080f6e155c4ead5842e1b9404784b7fdae
Luis Pomare <luispomare92@gmail.com>
2026-01-03 09:08:14 -0500

    Create closure from a method practice

--- JavaScript/experiments/closure-from-a-method-practice.js
+++ JavaScript/experiments/closure-from-a-method-practice.js
@@ -1,7 +1,16 @@
 function linearFunctionCreator(m, b) {
-  return function functionWithClosures(value) {
-    return m * Value + b
+  return {
+    functionWithClosures: function (value) {
+      return m * value + b
+    }
   }
 }
 
-const unitLinear = linearFunctionCreator(1, 0)
+const caller = linearFunctionCreator(1, 0)
+
+const unitLinear = caller.functionWithClosures
+
+console.log(unitLinear(3))
+console.log(unitLinear(4))
+console.log(unitLinear(5))
+
