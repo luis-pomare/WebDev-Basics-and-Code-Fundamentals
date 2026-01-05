4aa983632c712e117013a03d310be824c04760af
Luis Pomare <luispomare92@gmail.com>
2026-01-04 23:35:23 -0500

    Add array properties FCC challenge

---
+++ JavaScript/challenges/adding-properties-to-an-object-fcc.js
@@ -0,0 +1,14 @@
+const foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+foods.bananas = 13
+foods["grapes"] = 35
+const lastFood = "strawberries"
+foods[lastFood] = 27
+// Only change code above this line
+
+console.log(foods);
