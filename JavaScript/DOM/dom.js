84fa15fbd31e76251ebb2fc7c9ae97c7d3dce258
Luis Pomare <luispomare92@gmail.com>
2026-01-08 08:42:56 -0500

    Modify DOM experiment to modify HTML button with each click

--- JavaScript/DOM/dom.js
+++ JavaScript/DOM/dom.js
@@ -3,16 +3,16 @@ const anchor = document.getElementById("front-end-anchor")
 
 function callReact() {
   anchor.innerHTML = '<a href="./pages/frontEndFrameworks/react.html">react</a>'
+  button.innerText = "FEF instead of React"
   button.removeEventListener("click", callReact)
   button.addEventListener("click", callFrameworks)
-  button.innerText("FEF instead of React")
 }
 
 function callFrameworks() {
-  anchor.innerHTML = '<a href="./pages/frontEndFrameworks.html">FEF</a>'
+  anchor.innerHTML = '<a href="./pages/frontEndFrameworks.html">Frontend frameworks</a>'
+  button.innerText = "React instead of FEF"
   button.removeEventListener("click", callFrameworks)
   button.addEventListener("click", callReact)
-  button.innerText("React instead of FEF")
 }
 
 button.addEventListener("click", callReact)
