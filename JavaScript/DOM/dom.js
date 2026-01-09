fef437f4a23439038f82a744515fd7c468e3583e
Luis Pomare <luispomare92@gmail.com>
2026-01-09 05:22:55 -0500

    Doccument DOM experiment

--- JavaScript/DOM/dom.js
+++ JavaScript/DOM/dom.js
@@ -1,7 +1,10 @@
+// First this retrieves the elements that will control the navigation behavior
 const button = document.getElementById("call-react-button")
 const anchor = document.getElementById("front-end-anchor")
 
+
 function callReact() {
+  // Updates the anchor and button content to point specifically to the React page
   anchor.innerHTML = '<a href="./pages/frontEndFrameworks/react.html">react</a>'
   button.innerText = "FEF instead of React"
   button.removeEventListener("click", callReact)
@@ -9,10 +12,12 @@ function callReact() {
 }
 
 function callFrameworks() {
+  // Updates the anchor and button content to point specifically to the Frontent frameworks page
   anchor.innerHTML = '<a href="./pages/frontEndFrameworks.html">Frontend frameworks</a>'
   button.innerText = "React instead of FEF"
   button.removeEventListener("click", callFrameworks)
   button.addEventListener("click", callReact)
 }
 
+// Makes the initial assignment of the callback for the correct event handler
 button.addEventListener("click", callReact)
