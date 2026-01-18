6b91e063649fe8948d8e72eb68b1e4969fa2decd
Luis Pomare <luispomare92@gmail.com>
2026-01-18 06:16:02 -0500

    Move error propagation from step 1 to step 2 promise chain challenge

--- JavaScript/challenges/promises_and_call_backs/Promise_chain_logic_ST_challenge.js
+++ JavaScript/challenges/promises_and_call_backs/Promise_chain_logic_ST_challenge.js
@@ -1,18 +1,18 @@
 function step1(value) {
-  let result = value + 1
-  if (result > 5) return Promise.reject("Value too large")
-  return Promise.resolve(result)
+  return Promise.resolve(value + 1)
 }
 
 function step2(value) {
-  return Promise.resolve(value * 2)
+  let result = value * 2
+  if (result > 5) return Promise.reject("Value too large")
+  return Promise.resolve(result)
 }
 
 function step3(value) {
   return Promise.resolve("Result: " + value)
 }
 
-step1(5)
+step1(1)
   .then(step2)
   .then(step3)
   .then(console.log)
