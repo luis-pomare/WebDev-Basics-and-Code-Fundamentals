70010c2e33a3e7eb7b5a44b929759578af83898d
Luis Pomare <luispomare92@gmail.com>
2026-01-19 07:39:35 -0500

    Create async function for array of async calls challenge

--- JavaScript/challenges/promises_and_call_backs/array_of_async_calls_challenge.js
+++ JavaScript/challenges/promises_and_call_backs/array_of_async_calls_challenge.js
@@ -1,10 +1,14 @@
 // We are going to build a function that passes all the numbers from 0 to the number that the user provides
 // to the delayWithDouble function and stores every result in an array showing with each iteration the current state 
 
-function delay(ms, value) {
+function delayWithDouble(ms, value) {
   return new Promise(resolve => {
-    setTimeout(() => resolve(value), ms)
+    setTimeout(() => resolve(value * 2), ms)
   })
 }
 
+async function arrayCreator() {
+  return await delayWithDouble(3000, 7)
+}
 
+arrayCreator().then(console.log)
